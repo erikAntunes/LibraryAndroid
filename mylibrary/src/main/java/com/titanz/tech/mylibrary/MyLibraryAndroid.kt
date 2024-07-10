522d5cbe2b9100cd6de2296ca1bf1eb3c87732b0
Erik Antunes <erik.antunes@live.com>
2024-07-10 17:07:13 -0300

    prenchendo lib com extensions gerais

--- mylibrary/src/main/java/com/titanz/tech/mylibrary/MyLibraryAndroid.kt
+++
@@ -1,7 +0,0 @@
-package com.titanz.tech.mylibrary
-
-class MyLibraryAndroid {
-    fun getGreeting(): String {
-        return "Deu Certo Mermão! Lib Funcionando!"
-    }
-}
---
+++ mylibrary/src/main/java/com/titanz/tech/mylibrary/NumberExtensionAndroid.kt
@@ -0,0 +1,15 @@
+package com.titanz.tech.mylibrary
+
+import java.text.NumberFormat
+import java.util.Locale
+
+/**
+ * Função para formatar Double para moeda local*/
+fun Double.formatToLocalMoney(language: String, country: String): String {
+    return try {
+        val currencyFormat = NumberFormat.getCurrencyInstance(Locale("language", "country"))
+        currencyFormat.format(this)
+    } catch (_: Exception) {
+        "Error on format To Local Money"
+    }
+}
