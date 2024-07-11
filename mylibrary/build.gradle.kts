a76353b5ba16bfc7099c4b311a2dc4d11cc38501
Erik Antunes <erik.antunes@live.com>
2024-07-10 21:47:26 -0300

    build 0.0.4

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.titanz.tech.mylibrary"
                 artifactId = "LibraryAndroid"
-                version = "0.0.3"
+                version = "0.0.4"
             }
         }
         repositories {
--- mylibrary/src/main/java/com/titanz/tech/mylibrary/feature/NumberFeature.kt
+++ mylibrary/src/main/java/com/titanz/tech/mylibrary/feature/NumberFeature.kt
@@ -8,7 +8,7 @@ class NumberFeature {
      * Função para formatar Double para moeda local*/
     fun formatToLocalMoney(moneyDouble: Double, language: String, country: String): String {
         return try {
-            val currencyFormat = NumberFormat.getCurrencyInstance(Locale("language", "country"))
+            val currencyFormat = NumberFormat.getCurrencyInstance(Locale(language, country))
             currencyFormat.format(this)
         } catch (_: Exception) {
             "Error on format to local money"
